9721acba9574fbfc08f032cf3553c42b3a2d94ac
eathren <nolan.braman@gmail.com>
2025-12-03 13:58:49 -0800

    images for first items

--- entities/enemies/imp/imp.gd
+++ entities/enemies/imp/imp.gd
@@ -10,9 +10,8 @@ var target: Node2D
 func _ready() -> void:
 	add_to_group("enemy")
 
-	# Set collision layer to Enemy (layer 1 = 2^0 = 1)
-	# This allows Area2D bullets to detect us via their collision_mask
-	collision_layer = 1
+	# Collision layer 1 (enemy body) set in scene
+	# Collision mask 16 (only walls) set in scene - enemies don't collide with players or each other
 
 	target = get_tree().get_first_node_in_group("trawler") as Node2D
 	if health != null:
